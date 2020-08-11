284e39dc1cd5bf4ec4b1654cb75e200aac4fa565
AndonMitev <mitevandon94@gmail.com>
2020-08-11 16:43:44 +0300

    Add compiled data in blockchain folder

--- src/blockchain/lottery.js
+++ src/blockchain/lottery.js
@@ -1,4 +1,4 @@
-import { abi } from "../bin/src/blockchain/contracts/Lottery.json";
+import { abi } from "./bin/src/blockchain/contracts/Lottery.json";
 
 const CONTRACT_ADDRESS = "0x696f5Be40c4c51ee0eBf98B96aA76ED116c8a87c";
 
